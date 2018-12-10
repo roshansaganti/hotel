5770da852ddfd55572b8a234c6dfd61f3c80e1a1
RS <38731416+roshansaganti@users.noreply.github.com>
2018-12-10 17:56:44 -0500

    Completed Chapter 1: Getting Started With Laravel

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,39 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return '<h3>Landon App Page</h3>';
+    // return view('welcome');
+});
+
+Route::get('/about', function () {
+  $response_arr = [];
+  $response_arr['author'] = 'BP';
+  $response_arr['version'] = '0.1.1';
+  return $response_arr;
+  // return '<h3>About</h3>';
+});
+
+Route::get('/home', function () {
+  $data = [];
+  $data['version'] = '0.1.1';
+  return view('welcome', $data);
+});
+
+Route::get('/di', 'ClientController@di');
+
+Route::get('/facades/db', function () {
+
+  return DB::select('SELECT * FROM table');
+});
+
+Route::get('/facades/encrypt', function () {
+
+  return Crypt::encrypt('123456789');
+});
+
+//eyJpdiI6IktObWI3dlwvWm9NY0hGS0tJOWRJVDFRPT0iLCJ2YWx1ZSI6IkVvZURQd1wvaVJlXC9oZGNnU3ZQUzNGQ2RxWlJjM3V5azBNTU1NZkYxQk90Yz0iLCJtYWMiOiI5ZGE2NjVhYTNmNWJjOTM2YTk3Mzg5ZjZhNmZlZjdiNmRmZjFlYmJlY2UzYTNiMzE5Y2RhZWJmYWJiZGIwNzgzIn0
+
+Route::get('/facades/decrypt', function () {
+
+  return Crypt::decrypt('eyJpdiI6IktObWI3dlwvWm9NY0hGS0tJOWRJVDFRPT0iLCJ2YWx1ZSI6IkVvZURQd1wvaVJlXC9oZGNnU3ZQUzNGQ2RxWlJjM3V5azBNTU1NZkYxQk90Yz0iLCJtYWMiOiI5ZGE2NjVhYTNmNWJjOTM2YTk3Mzg5ZjZhNmZlZjdiNmRmZjFlYmJlY2UzYTNiMzE5Y2RhZWJmYWJiZGIwNzgzIn0');
 });
