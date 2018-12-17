82c16a47a3adcfa500ee0e7cb2d1b33cd15e7b16
RS <38731416+roshansaganti@users.noreply.github.com>
2018-12-17 12:19:41 -0500

    Completed Chapter 3: Views and Blade

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -7,10 +7,9 @@ use App\Title as Title;
 
 class ClientController extends Controller
 {
-    //
     public function __construct( Title $titles )
     {
-        $this->titles = $titles->all();
+        $this->titles           = $titles->all();
     }
 
     public function di()
@@ -20,12 +19,65 @@ class ClientController extends Controller
 
     public function index()
     {
-        return view('client/index');
+        $data = [];
+
+        $obj                    = new \stdClass;
+        $obj->id                = 1;
+        $obj->title             = 'mr';
+        $obj->name              = 'john';
+        $obj->last_name         = 'doe';
+        $obj->email             = 'john@domain.com';
+
+        $data['clients'][] = $obj;
+
+        $obj                    = new \stdClass;
+        $obj->id                = 2;
+        $obj->title             = 'ms';
+        $obj->name              = 'jane';
+        $obj->last_name         = 'doe';
+        $obj->email             = 'jane@another-domain.com';
+
+        $data['clients'][]      = $obj;
+
+        return view('client/index', $data);
     }
 
-    public function newClient()
+    public function newClient(Request $request)
     {
-        return view('client/newClient');
+        $data = [];
+
+        $data['title']          = $request->input('title');
+        $data['name']           = $request->input('name');
+        $data['last_name']      = $request->input('last_name');
+        $data['address']        = $request->input('address');
+        $data['zip_code']       = $request->input('zip_code');
+        $data['city']           = $request->input('city');
+        $data['state']          = $request->input('state');
+        $data['email']          = $request->input('email');
+
+        $data['titles']         = $this->titles;
+        $data['modify']         = 0;
+
+        if ($request->isMethod('post'))
+        {
+            // dd($data);
+            $this->validate(
+                $request,
+                [
+                    'name' => 'required|min:5',
+                    'last_name' => 'required',
+                    'address' => 'required',
+                    'zip_code' => 'required',
+                    'city' => 'required',
+                    'state' => 'required',
+                    'email' => 'required',
+
+                ]
+            );
+            return redirect('clients');
+        }
+
+        return view('client/form', $data);
     }
 
     public function create()
@@ -35,6 +87,9 @@ class ClientController extends Controller
 
     public function show($client_id)
     {
-        return view('client/show');
+        $data                   = [];
+        $data['titles']         = $this->titles;
+        $data['modify']         = 1;
+        return view('client/form', $data);
     }
 }
