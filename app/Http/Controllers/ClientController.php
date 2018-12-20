bce56977f4fc8b8fa1a688717aaf70cdc58aad36
RS <38731416+roshansaganti@users.noreply.github.com>
2018-12-19 19:41:29 -0500

    Completed Chapter 4: Introducing Eloquent
    - This is also the conclusion of part 1 of the Laravel 5 Essential Training series: The Basics
    - Next commit will include the first chapter of the second part of the series: Testing, Securing, and Deploying

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Title as Title;
+use App\Client as Client;
 
 class ClientController extends Controller
 {
-    public function __construct( Title $titles )
+    public function __construct(Title $titles, Client $client)
     {
         $this->titles           = $titles->all();
+        $this->client           = $client;
     }
 
     public function di()
@@ -21,28 +23,14 @@ class ClientController extends Controller
     {
         $data = [];
 
-        $obj                    = new \stdClass;
-        $obj->id                = 1;
-        $obj->title             = 'mr';
-        $obj->name              = 'john';
-        $obj->last_name         = 'doe';
-        $obj->email             = 'john@domain.com';
+        $data['clients'] = $this->client->all();
 
-        $data['clients'][] = $obj;
-
-        $obj                    = new \stdClass;
-        $obj->id                = 2;
-        $obj->title             = 'ms';
-        $obj->name              = 'jane';
-        $obj->last_name         = 'doe';
-        $obj->email             = 'jane@another-domain.com';
-
-        $data['clients'][]      = $obj;
+        // $data['clients'][]      = $obj;
 
         return view('client/index', $data);
     }
 
-    public function newClient(Request $request)
+    public function newClient(Request $request, Client $client)
     {
         $data = [];
 
@@ -54,9 +42,6 @@ class ClientController extends Controller
         $data['city']           = $request->input('city');
         $data['state']          = $request->input('state');
         $data['email']          = $request->input('email');
-
-        $data['titles']         = $this->titles;
-        $data['modify']         = 0;
 
         if ($request->isMethod('post'))
         {
@@ -74,8 +59,13 @@ class ClientController extends Controller
 
                 ]
             );
+
+            $client->insert($data);
+
             return redirect('clients');
         }
+        $data['titles']         = $this->titles;
+        $data['modify']         = 0;
 
         return view('client/form', $data);
     }
@@ -88,8 +78,69 @@ class ClientController extends Controller
     public function show($client_id)
     {
         $data                   = [];
+        $data['client_id']      = $client_id;
         $data['titles']         = $this->titles;
         $data['modify']         = 1;
+        $client_data = $this->client->find($client_id);
+        $data['name'] = $client_data->name;
+        $data['last_name'] = $client_data->last_name;
+        $data['title'] = $client_data->title;
+        $data['address'] = $client_data->address;
+        $data['zip_code'] = $client_data->zip_code;
+        $data['city'] = $client_data->city;
+        $data['state'] = $client_data->state;
+        $data['email'] = $client_data->email;
         return view('client/form', $data);
     }
+
+    public function modify(Request $request, $client_id, Client $client)
+    {
+        $data = [];
+
+        $data['title']          = $request->input('title');
+        $data['name']           = $request->input('name');
+        $data['last_name']      = $request->input('last_name');
+        $data['address']        = $request->input('address');
+        $data['zip_code']       = $request->input('zip_code');
+        $data['city']           = $request->input('city');
+        $data['state']          = $request->input('state');
+        $data['email']          = $request->input('email');
+
+        if ($request->isMethod('post'))
+        {
+            // dd($data);
+            $this->validate(
+                $request,
+                [
+                    'name' => 'required|min:5',
+                    'last_name' => 'required',
+                    'address' => 'required',
+                    'zip_code' => 'required',
+                    'city' => 'required',
+                    'state' => 'required',
+                    'email' => 'required',
+
+                ]
+            );
+
+            $client_data = $this->client->find($client_id);
+            $client_data->title = $request->input('title');
+            $client_data->name = $request->input('name');
+            $client_data->last_name = $request->input('last_name');
+            $client_data->address = $request->input('address');
+            $client_data->zip_code = $request->input('zip_code');
+            $client_data->city = $request->input('city');
+            $client_data->state = $request->input('state');
+            $client_data->email = $request->input('email');
+
+            $client_data->save();
+
+            return redirect('clients');
+        }
+        $data['titles']         = $this->titles;
+        $data['modify']         = 0;
+
+        return view('client/form', $data);
+    }
+
 }
