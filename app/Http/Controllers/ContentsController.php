ca86834768cd72578f5490aeb7cb15742519b9c4
RS <38731416+roshansaganti@users.noreply.github.com>
2018-12-26 12:06:14 -0500

    Completed Chapter 3: Beyond the Basics

--- app/Http/Controllers/ContentsController.php
+++ app/Http/Controllers/ContentsController.php
@@ -3,10 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
 
 class ContentsController extends Controller
 {
-    //
     public function home(Request $request)
     {
         $data = [];
@@ -14,5 +14,21 @@ class ContentsController extends Controller
         $last_updated = $request->session()->has('last_updated') ? $request->session()->pull('last_updated') : 'none';
         $data['last_updated'] = $last_updated;
         return view('contents/home', $data);
+    }
+    public function upload(Request $request)
+    {
+        $data = [];
+        if($request->isMethod('post'))
+        {
+            $this->validate(
+                $request,
+                [
+                    'image_upload' => 'mimes:jpeg,bmp,png'
+                ]
+            );
+            Input::file('image_upload')->move('images', 'attractions.jpg');
+            return redirect('/');
+        }
+        return view('contents/upload', $data);
     }
 }
